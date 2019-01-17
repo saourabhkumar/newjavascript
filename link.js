194f6ec8a720f3dec85b3e2369ab3dd5a8ec9c11
Saourabh Kumar <46557794+saourabhkumar@users.noreply.github.com>
2019-01-17 03:38:44 -0800

    Add files via upload

---
+++ link.js
@@ -0,0 +1,4 @@
+
+document.write("<h1 style='background-color:yellow;color:blue'>Welcome to Externaljava Script..!</h1>");
+document.write("<br/>");
+document.write("Thank u...");
---
+++ new.js
@@ -0,0 +1,7 @@
+<!doctype html>
+<head>
+<script type="text/javascript" language="javascript">
+document.write("Welcome to javaScript");
+</script>
+</head>
+</html>
